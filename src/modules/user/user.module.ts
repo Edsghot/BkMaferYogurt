31aeaa97393b6317f1fc84d7565236c05ad92b79
erick <edsghot@gmail.com>
2024-07-23 18:51:38 -0500

    se agrego validateService a user

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,12 +3,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/UserEntity.entity';
+import { ValidateService } from 'src/Common/Validate/validate.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User])
   ],
-  providers: [UserService],
+  providers: [UserService,ValidateService],
   controllers: [UserController]
 })
 export class UserModule {}
