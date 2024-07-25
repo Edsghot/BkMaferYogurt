cebb16e6cd495cecdb223caee00ecfbbb3a532fd
erick <edsghot@gmail.com>
2024-07-25 00:04:26 -0500

    se agrego referencias de cart

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -1,6 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from '../product/entity/ProductEntity.entity';
+import { CreateProductRequest } from '../product/request/CreateProductRequest.request';
+import { Cart } from './entity/CartEntity.entity';
+import { CartItem } from './entity/CartItem.entity';
+import { CreateCartRequest } from './request/CreateCartRequest';
+import { User } from '../user/entity/UserEntity.entity';
 
 @Injectable()
 export class CartService {
+    constructor(
+        @InjectRepository(Cart)
+        private readonly productRepository: Repository<Cart>,
+        @InjectRepository(CartItem)
+        private readonly cartItemRepository: Repository<CartItem>,
+
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+      ) {}
     
+      async insertProduct(request: CreateCartRequest) {
+        try {
+            
+            
+
+
+        } catch (error) {
+          return { msg: 'Error al insertar producto', detailMsg: error.message, success: false };
+        }
+      }
 }
