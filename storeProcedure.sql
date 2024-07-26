11ade4fd9b756c6e4a5e25fe06ea267a02a593b6
AlexMachaca <212129@unamba.edu.pe>
2024-07-25 19:20:18 -0500

    Store Precedure.sql

---
+++ storeProcedure.sql
@@ -0,0 +1,10 @@
+DELIMITER //
+CREATE PROCEDURE getUserByDateRange(IN Inicio DATE, IN Fin DATE) 
+BEGIN 
+	SELECT u.FirstName,u.LastName,u.Dni, u.Address,u.Phone,u.Mail,u.BirthDate
+	FROM user u
+	WHERE u.Rol=0 AND u.DateCreated >= Inicio AND u.DateCreated <= Fin; 
+END//
+DELIMITER ;
+
+CALL getUserByDateRange('2024-07-01', '2024-07-30');
