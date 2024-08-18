271b1bf0f36639475ddd662d0cb36a9982423da8
edsghot <edsghot@gmail.com>
2024-08-18 16:57:43 -0500

    se modifico el correo de administrador

--- src/Common/constants/constantService.ts
+++ src/Common/constants/constantService.ts
@@ -1,4 +1,4 @@
 export const CLOUDINARY = 'Cloudinary'
 export const FOLDER_PAYMENT = 'MaferYogurt/Product'
 export const CHAT_USER = 'Chat/User'
-export const CORREO = 'cochadeu@gmail.com'
+export const CORREO = '212129@unamba.edu.pe'
